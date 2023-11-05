c68cf99f90eb443d091a8d1877ef18e9e4acc690
maxoliverbr <max.oliver@cintrax.com.br>
2023-11-05 13:02:44 -0300

    Fixed params in fixture

--- conftest.py
+++ conftest.py
@@ -1,21 +1,25 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.remote.remote_connection import RemoteConnection
+#from selenium.webdriver.remote.remote_connection import RemoteConnection
 from utilities.test_data import TestData
+
 
 """
 Fixture to open the browser instance for each test case.
 """
 
 
-@pytest.fixture(params=["chrome, firefox"])
+@pytest.fixture(params=["chrome", "firefox"])
 def initialize_driver(request):
+
     if request.param == "chrome":
         driver = webdriver.Chrome()
     elif request.param == "firefox":
         driver = webdriver.Firefox()
     elif request.param == "edge":
         driver = webdriver.Edge()
+    else:
+        driver = webdriver.Chrome()
 
     driver.implicitly_wait(5)
     request.cls.driver = driver
